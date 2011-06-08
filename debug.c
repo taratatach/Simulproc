281714ca6459ccd5562f8fe612b059068f5fe45b
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2011-06-08 13:20:19 +0200

    version encore mieux, avec l'entrée détectée :)

--- debug.c
+++ debug.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "instruction.h"
 #include "machine.h"
 #include "exec.h"
@@ -8,49 +9,63 @@ bool debug_ask(Machine *pmach){
 
     //le tableau de char de réponse
     char tab[101] = {0};
-    char c;
     //on demande de taper quelque chose
     int ok = 0; //indique si on a besoin de redemander de taper qqch
     do{
         printf("DEBUG? ");
-        scanf("%100s", tab);
-        //help
-        if(strcmp(tab, "h")==0){
-            printf("Available commands:\n");
-            printf("\th\thelp\n");
-            printf("\tc\tcontinue (exit interactive debug mode)\n");
-            printf("\ts\tstep by step (next instruction)\n");
-            printf("\tRET\tstep by step (next instruction)\n");
-            printf("\tr\tprint registers\n");
-            printf("\td\tprint data memory\n");
-            printf("\tt\tprint text (program) memory\n");
-            printf("\tp\tprint text (program) memory\n");
-            printf("\tm\tprint registers and data memory\n");
-        }
-        //continue (exit interactive debug mode)
-        else if(strcmp(tab, "c")==0){
-            return false;
-        }
-        //step by step (next instruction)
-        else if(strcmp(tab, "s")==0 || strcmp(tab, "")==0){
+        fgets (tab, 100, stdin);
+        
+        //Si on a appuyé sur Entrée directement
+        if(tab[0] == '\n'){
             return true;
         }
-        //print registers
-        else if(strcmp(tab, "r")==0){
-            print_cpu(pmach);
+        //Sinon on supprime le \n
+        else{
+            char *c;
+            c = strchr(tab, '\n');
+            c[0] = 0;
         }
-        //print data memory
-        else if(strcmp(tab, "d")==0){
-            print_data(pmach);
-        }
-        //print text (program) memory
-        else if(strcmp(tab, "t")==0 || strcmp(tab, "p")==0){
-            print_program(pmach);
-        }
-        //print registers and data memory
-        else if(strcmp(tab, "m")==0){
-            print_cpu(pmach);
-            print_data(pmach);
+        switch(tab[0]){
+            //help
+            case 'h': 
+                printf("Available commands:\n");
+                printf("\th\thelp\n");
+                printf("\tc\tcontinue (exit interactive debug mode)\n");
+                printf("\ts\tstep by step (next instruction)\n");
+                printf("\tRET\tstep by step (next instruction)\n");
+                printf("\tr\tprint registers\n");
+                printf("\td\tprint data memory\n");
+                printf("\tt\tprint text (program) memory\n");
+                printf("\tp\tprint text (program) memory\n");
+                printf("\tm\tprint registers and data memory\n");
+                break;
+            //continue (exit interactive debug mode)
+            case 'c':
+                return false;
+            //step by step (next instruction)
+            case 's':
+                return true;
+            //print registers
+            case 'r':
+                print_cpu(pmach);
+                break;
+            //print data memory
+            case 'd':
+                print_data(pmach);
+                break;
+            //print text (program) memory
+            case 't':
+                print_program(pmach);
+                break;
+            //print text (program) memory
+            case 'p':
+                print_program(pmach);
+                break;
+            //print registers and data memory
+            case 'm': 
+                print_cpu(pmach);
+                print_data(pmach);
+                break;
         }
     }while(!ok);
     
